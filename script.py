1f37d830783379269817ac691a45e02c8fd86fa5
MalMWare <malkelleymartin@gmail.com>
2023-05-31 23:05:24 -0700

    created a line graph using matplotlib

--- script.py
+++ script.py
@@ -1,9 +1,30 @@
 import requests
 import pprint 
-from matplotlib import pyplot as plit
+#import json
+from matplotlib import pyplot as plt
 from datetime import datetime
 
-API_URL = "https://rickandmortyapi.com/api/character/?status=alive"
+API_URL = "https://rickandmortyapi.com/api/episode"
 r = requests.get(API_URL)
 resp = r.json()
-print(resp)
+pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(resp)
+
+episodes = {}
+for res in resp['results']:
+    episodes[res["name"]] = len(res["characters"])
+
+pp.pprint(episodes)
+
+
+#json dumps way 
+#episodes = {}
+#for res in resp['results]:
+    #episodes[res["name"]] = len(res["characters"])
+#print(json.dumps(episode, indent=4))
+
+plt.xlabel('episode name')
+plt.xticks(rotation=90)
+plt.ylabel('character count')
+plt.plot(episodes.keys(), episodes.values())
+plt.show()
